37bee6a713f651736f33e2f3827e5154ec74c1a2
Wellington Campos <wellington.albertoni@gmail.com>
2022-01-04 09:23:31 -0400

    Bugs fixes and minor improvements
    - Change formatation of strings to fstring
    - Create a credentials file
    - Rename CHANNEL_ID var to CHAT_TARGET

--- artic.py
+++ artic.py
@@ -1,9 +1,7 @@
 import re
 import requests
 from time import sleep
-
-BOT_TOKEN = 'INSERT_BOT_TOKEN_HERE'
-CHANNEL_ID = 'INSERT CHANNEL ID OR USERNAME'
+from credentials import BOT_TOKEN, CHAT_TARGET
 
 API_URL = 'https://api.telegram.org/bot' + BOT_TOKEN + '/'
 
@@ -19,20 +17,15 @@ def post_article(id_, title, time, content, url, delay=10):
     content = re.sub(pattern, r'\\\1', content)
     time = re.sub(r'\-', r'\\-', time)
 
-    message ="*{titulo}*\n\n{datetime}\n\n_{content}_\n\n[LEIA MAIS]({url})\n\n\#post\_{ID}".format(
-        titulo = title, 
-        datetime = time, 
-        content = content, 
-        url = url, 
-        ID= id_
-    )
-    r = requests.get(api_url + 'sendMessage', data={"chat_id":CHAT_TARGET,"text":message, "parse_mode":"MarkdownV2"})
+    message = f"*{title}*\n\n{time}\n\n_{content}_\n\n[LEIA MAIS]({url})\n\n\#post\_{id_}"
+
+    r = requests.get(API_URL + 'sendMessage', data={"chat_id":CHAT_TARGET,"text":message, "parse_mode":"MarkdownV2"})
     response = r.json()
 
     if(response['ok'] is True):
-        print("[SUCCESSFUL] REQUEST POST ID: {}\n".format(ID))
+        print(f"[SUCCESSFUL] REQUEST POST ID: {id_}\n")
     else:
-        print("[FAILED] REQUEST POST ID: {}\n".format(ID))
+        print(f"[FAILED] REQUEST POST ID: {id_}\n")
 
     sleep(delay)
 
---
+++ credentials.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = 'Insert bot token here'
+CHAT_TARGET = 'ID or @username of target chat'
