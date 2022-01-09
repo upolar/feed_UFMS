0d1f511e1a842998ec13f35b2a2d046c645552f6
Wellington Campos <wellington.albertoni@gmail.com>
2022-01-08 21:43:56 -0400

    Change project structure and minor improvements
    - Create /app and /modules folders
    - Restructure of main and articles
    - Use bot class in articles.py instead of a function
    - Bug fixes and minor improvements

--- app/modules/articles.py
+++ app/modules/articles.py
@@ -1,17 +1,16 @@
 import re
-import requests
 from time import sleep
-from app.credentials import BOT_TOKEN, CHAT_TARGET
 
-API_URL = 'https://api.telegram.org/bot' + BOT_TOKEN + '/'
+from credentials import BOT_TOKEN, CHAT_TARGET
+
+from modules.bot import Bot
+
+bot = Bot(BOT_TOKEN)
 
 def exist_article(data, value):
     return any(new==value for new in data)
 
-def create_article_list(id_):
-    return new_data.append(id_)
-
-def post_article(id_, title, time, content, url, delay=10):
+def format_post_content(id_, title, time, content, url):
     pattern = r'([\_|\*|\[|\]|\(|\)|\~|\`|\>|\#|\+|\-|\=|\||\{|\}|\.|\!])'
     title = re.sub(pattern, r'\\\1', title)
     content = re.sub(pattern, r'\\\1', content)
@@ -19,9 +18,10 @@ def post_article(id_, title, time, content, url, delay=10):
 
     message = f"*{title}*\n\n{time}\n\n_{content}_\n\n[LEIA MAIS]({url})\n\n\#post\_{id_}"
 
-    s = requests.Session()
-    r = s.get(API_URL + 'sendMessage', data={"chat_id":CHAT_TARGET,"text":message, "parse_mode":"MarkdownV2"})
-    response = r.json()
+    return message
+
+def create_post(msg, delay=10):
+    bot.send_message(CHAT_TARGET, msg)
 
     sleep(delay)
 
